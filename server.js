a2ba54f716295ce8e36379ee45d461f73e03b5ca
Pitla Sai Krishna <108579133+Pitlakrishna@users.noreply.github.com>
2024-05-01 20:28:25 +0530

    server.js

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ import colors from "colors"
 import ConnectDB from "./config/db.js"
 import morgan from "morgan"
 import authRoute from "./Routes/authRoute.js"
+import path from 'path'
+import { fileURLToPath } from "url";
 
 
 dotEnv.config()
 ConnectDB()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, "./client/build")))
 
 app.use('/api/v1/task', authRoute)
+
+app.use('*', function (req, res) {
+     res.sendFile(path.join(__dirname, './client/build/index.html'))
+ })
 
 const PORT = process.env.PORT || 8000;
 
